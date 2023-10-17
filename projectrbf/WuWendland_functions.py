f92caf07fa656dd63e8cf2d4777ae0873aca1aea
dlaparna7 <140335752+dlaparna7@users.noreply.github.com>
2023-10-17 16:50:43 +0200

    Add files via upload

---
+++ projectrbf/WuWendland_functions.py
@@ -0,0 +1,45 @@
+def wu0(r):
+    k = 0
+    l = k+2 #floor(d/2) + k + 1
+    if (r <= 1):
+        phi = (1-r**2)
+    else:
+        phi = 0
+    return phi
+def wendland0(r):
+    k = 0
+    l = k+2 #floor(d/2) + k + 1
+    if (r <= 1):
+        phi = (1-r)**l
+    else:
+        phi = 0
+    return phi
+
+def wendland1(r):
+    k = 1
+    l = k+2
+    if (r <= 1):
+        phi = (((1-r)**(l+1))*((l+1)*r +1))
+    else:
+        phi = 0
+    return phi
+
+def wendland2(r):
+    k=2
+    l=k+2
+    if (r <= 1):
+        phi = ((1-r)**(l+2))*(((l**2)+(4*l)+3)*(r**2) +(3*l + 6)*r + 3)
+    else:
+        phi = 0
+    return phi
+
+def wendland3(r):
+    k=3
+    l=k+2
+
+    if (r <= 1):
+        phi = ((1-r)**(l+3))*(((l**3) + 9*(l**2) + 23*l + 15)*(r**3) + (6*(l**2) + 36*l + 45)*(r**2) + (15*l + 45)*r + 15 )
+    else:
+        phi = 0
+    return phi
+
