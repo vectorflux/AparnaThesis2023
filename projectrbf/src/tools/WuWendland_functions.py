10b6b9137b88d548f6600ba2d263a8cd59a92064
Aparna Devulapalli <140335752+dlaparna7@users.noreply.github.com>
2024-03-26 14:26:37 +0100

    Updated WuWendland_functions.py with new derivates for higher order functions

--- projectrbf/src/tools/WuWendland_functions.py
+++ projectrbf/src/tools/WuWendland_functions.py
@@ -43,7 +43,7 @@ def wendland3(r):
         phi = 0
     return phi
 
-def wendland0_prime(r):
+def wendland0_prime(r): #Derivative of Wendland 0 with k =0 , l = 2
 
     if (r <= 1):
         phi_p = -2*(1 - r)
@@ -52,7 +52,7 @@ def wendland0_prime(r):
     return phi_p
 
 
-def wendland1_prime(r):
+def wendland1_prime(r): #Derivative of Wendland 1 with k =1 , l = 3
 
     if (r <= 1):
         #phi_p = -16*(r**3) + 33*(r**2) - 18*r +1
@@ -61,10 +61,27 @@ def wendland1_prime(r):
         phi_p = 0
     return phi_p
 
-def wendland_3_1_prime(r):
+def wendland_3_1_prime(r): #Derivative of Wendland 1 with k =1 , l = 3
 
     if (r <= 1):
         phi_p = 20*(r**4) - 60*(r**3) + 60*(r**2) - 20*r 
+    else:
+        phi_p = 0
+    return phi_p
+
+def wendland2_prime(r): #Derivative of Wendland 2 with k =2 , l = 4
+
+    if (r <= 1):
+        phi_p = 280*(r**7) - 1344*(r**6) + 2520*(r**5) - 2240*(r**4) + 840*(r**3) - 56*r
+
+    else:
+        phi_p = 0
+    return phi_p
+
+def wendland3_prime(r): #Derivative of Wendland 3 with k =3 , l = 5
+
+    if (r <= 1):
+        phi_p = 5280*(r**10) - 34650*(r**9) + 95040*(r**8) - 138600*(r**7) + 110880*(r**6) - 41580*(r**5) + 3960*(r**3) - 330*r
     else:
         phi_p = 0
     return phi_p
